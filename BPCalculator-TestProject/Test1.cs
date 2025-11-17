b6be201d92f17b27a0bfd2b3899202add2d9e704
seshukalakurthi <seshubabu.kalakurthi@gmail.com>
2025-11-17 11:16:21 +0000

    Created MS Test Project

---
+++ BPCalculator-TestProject/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace BPCalculator_TestProject
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
